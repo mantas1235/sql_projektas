7b0370db87318860b63ad01b69eb0289575c96da
Mantas Santockis <mantas.san@gmail.com>
2022-08-16 11:04:06 +0300

    New port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 
 const database = await mysql.createConnection({
